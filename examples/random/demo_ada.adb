bc068b7a46826cabb2a3333f754e65262898d6c7
jrmarino <draco@marino.st>
2016-07-17 19:38:32 -0500

    Fix blake2b record for x86_64
    The IC.size_t type varies between 32-bit and 64-bit systems, so defining
    the record by bit sizes failed on FreeBSD amd64.  Just go back to pack
    pragma.
    
    All test cases continue to pass on windows and now on FreeBSD64 10.3.

--- examples/random/demo_ada.adb
+++ examples/random/demo_ada.adb
@@ -13,8 +13,8 @@ begin
 
    Put_Line ("    Full random: " & Random_Word'Img);
    Put_Line ("Limited to $FFF: " & Random_Limited_Word (upper_bound)'Img);
-   Put_Line ("    Random salt: " & Random_Salt);
-   Put_Line ("   Random short: " & Random_Short_Key);
-   Put_Line (" Random Std key: " & Random_Standard_Hash_Key);
-   Put_LIne ("Rand 48-bit Key: " & Random_Hash_Key (Custom_Size));
+   Put_Line ("    Random salt: " & As_Hexidecimal (Random_Salt));
+   Put_Line ("   Random short: " & As_Hexidecimal (Random_Short_Key));
+   Put_Line (" Random Std key: " & As_Hexidecimal (Random_Standard_Hash_Key));
+   Put_LIne ("Rand 48-bit Key: " & As_Hexidecimal (Random_Hash_Key (Custom_Size)));
 end Demo_Ada;
